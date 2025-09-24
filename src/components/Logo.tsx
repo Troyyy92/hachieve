f6fbdc8c57b223af8bd4ca124f923c33a2670b95
[dyad] <robintardieu92340@hotmail.fr>
2025-09-24 23:18:06 +0200

    [dyad] wrote 1 file(s)

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -12,11 +12,11 @@ export const Logo = ({ className, size = 40 }: LogoProps) => {
       width={size} 
       height={size} 
       viewBox="0 0 100 100" 
-      fill="none" 
+      // Removed fill="none" from here
       xmlns="http://www.w3.org/2000/svg" 
       className={cn(className)}
     >
-      <path d="M50 0L61.2257 34.5491H97.5528L68.1636 55.4509L79.3893 90.L50 69.0983L20.6107 90L31.8364 55.4509L2.44717 34.5491H38.7743L50 0Z" fill="#4A5FE8"/>
+      <path d="M50 0L61.2257 34.5491H97.5528L68.1636 55.4509L79.3893 90.L50 69.0983L20.6107 90L31.8364 55.4509L2.44717 34.5491H38.7743L50 0Z" fill="currentColor"/> {/* Changed fill to currentColor */}
     </svg>
   );
 };
