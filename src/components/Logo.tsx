0ccfdc8788fd2056893cfa12bc76df5eb8de6cb1
[dyad] <robintardieu92340@hotmail.fr>
2025-09-24 23:39:43 +0200

    [dyad] wrote 1 file(s)

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -11,23 +11,53 @@ export const Logo = ({ className, size = 40 }: LogoProps) => {
     <svg 
       width={size} 
       height={size} 
-      viewBox="0 0 100 100" 
+      viewBox="0 0 450 450" 
       xmlns="http://www.w3.org/2000/svg" 
       className={cn(className)}
     >
-      {/* Octagonal outline */}
-      <path 
-        d="M 50 5 L 85 15 L 95 50 L 85 85 L 50 95 L 15 85 L 5 50 L 15 15 Z" 
-        fill="none" 
-        stroke="currentColor" 
-        strokeWidth="5" 
-        strokeLinejoin="round"
-      />
-      {/* Filled central square */}
-      <path 
-        d="M 35 35 L 65 35 L 65 65 L 35 65 Z" 
-        fill="currentColor"
-      />
+      {/* Octogone extérieur avec coins arrondis */}
+      <g>
+        {/* Fond bleu de l'octogone avec coins arrondis */}
+        <path d="M 160 60
+                 L 290 60
+                 Q 300 60 307 67
+                 L 383 143
+                 Q 390 150 390 160
+                 L 390 290
+                 Q 390 300 383 307
+                 L 307 383
+                 Q 300 390 290 390
+                 L 160 390
+                 Q 150 390 143 383
+                 L 67 307
+                 Q 60 300 60 290
+                 L 60 160
+                 Q 60 150 67 143
+                 L 143 67
+                 Q 150 60 160 60
+                 Z" 
+              fill="#2443cc"/>
+        
+        {/* Trou intérieur */}
+        <path d="M 165 90
+                 L 285 90
+                 L 360 165
+                 L 360 285
+                 L 285 360
+                 L 165 360
+                 L 90 285
+                 L 90 165
+                 Z" 
+              fill="white"/>
+      </g>
+      
+      {/* Carré central aligné avec les segments de l'octogone */}
+      <rect x="165" y="165" 
+            width="120" 
+            height="120" 
+            rx="15"
+            ry="15"
+            fill="#2443cc"/>
     </svg>
   );
 };
