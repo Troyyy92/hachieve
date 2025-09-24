f9d76fa4f3b2ac891a88c6c2bb2364f7f95bcbe0
[dyad] <robintardieu92340@hotmail.fr>
2025-09-24 23:20:47 +0200

    [dyad] wrote 1 file(s)

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -12,11 +12,15 @@ export const Logo = ({ className, size = 40 }: LogoProps) => {
       width={size} 
       height={size} 
       viewBox="0 0 100 100" 
-      // Removed fill="none" from here
       xmlns="http://www.w3.org/2000/svg" 
       className={cn(className)}
     >
-      <path d="M50 0L61.2257 34.5491H97.5528L68.1636 55.4509L79.3893 90.L50 69.0983L20.6107 90L31.8364 55.4509L2.44717 34.5491H38.7743L50 0Z" fill="currentColor"/> {/* Changed fill to currentColor */}
+      {/* Path for the octagonal shape with a square inside */}
+      <path 
+        d="M 50 5 L 85 15 L 95 50 L 85 85 L 50 95 L 15 85 L 5 50 L 15 15 Z M 35 35 L 65 35 L 65 65 L 35 65 Z" 
+        fill="currentColor" 
+        fillRule="evenodd"
+      />
     </svg>
   );
 };
